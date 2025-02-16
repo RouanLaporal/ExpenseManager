53b5cbfaf856bd803e62f9359356c731e33533a0
Rouan Laporal <rouan.laporal@gmail.com>
2025-02-16 21:11:34 +0100

    structural: Set up swagger.io

--- src/Infrastructure/Symfony/Controller/UserController.php
+++ src/Infrastructure/Symfony/Controller/UserController.php
@@ -10,10 +10,40 @@ use Domain\UseCase\User\CreateUserUseCase;
 use Infrastructure\Symfony\Presenters\User\CreateUserPresenterToJson;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use OpenApi\Attributes as OA;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Property;
 
 class UserController extends AbstractController{
     
-    #[Route('/user/create', name: 'create_user')]
+    #[Route('/api/user/create', name: 'create_user', methods: ['POST'])]
+    #[OA\RequestBody(
+        required: true,
+        content: new OA\JsonContent(
+            type: 'object',
+            properties: [
+                new OA\Property(property: 'id', type: 'integer'),
+                new OA\Property(property: 'firstName', type: 'string'),
+                new OA\Property(property: 'lastName', type: 'string'),
+                new OA\Property(property: 'email', type: 'string'),
+                new OA\Property(property: 'password', type: 'string')
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'User created successfully',
+        content: new JsonContent(
+            type:'string',
+            properties: [
+                new Property(property:'message', type: 'string', example:'User created successfully')
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 400,
+        description: 'Invalid input'
+    )]
     public function createAction(
         Request $request, 
         IUserGateway $userGateway,
